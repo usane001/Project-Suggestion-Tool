47b61f52ec62f7497051abde71e042ebd3e98b02
Umayr Shaikh <umayrshaikh@hotmail.com>
2023-02-22 20:52:20 +0000

    Update database

--- docs/includes/signup.inc.php
+++ docs/includes/signup.inc.php
@@ -8,12 +8,36 @@ if (isset($_POST["submit"])) {
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["pwdrepeat"];
 
-    require_once 'dbh.inc.php'
-    require_once ''
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $username, $email, $pwd, $pwdRepeat) !== false) {
+        header("location: ../Signup.php?error=emptyinput");
+        exit();
+    }
+    if (InvalidUid($username) !== false) {
+        header("location: ../Signup.php?error=invaliduid");
+        exit();
+    }
+    if (InvalidEmail($email) !== false) {
+        header("location: ../Signup.php?error=invalidemail");
+        exit();
+    }
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+        header("location: ../Signup.php?error=passwordsdontmatch");
+        exit();
+    }
+    if (uidExists($conn, $username, $email) !== false) {
+        header("location: ../Signup.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd);
 
 }
 else {
     header("location: ../Signup.php");
+    exit();
 
 }
 
