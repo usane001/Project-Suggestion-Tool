911275ad0ed6c6d26ef59ba9f738103db98c458d
Umayr Shaikh <umayrshaikh@hotmail.com>
2023-04-11 23:11:52 +0100

    Unnecessary files deletion
    Unwanted

--- docs/Pages/cancel_proj.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../Pages/includes/dbh.inc.php';
-
-$query= mysqli_query($conn,"DELETE from projects where project_id = '".$_GET['id']."'");
-if($query){
-	header("location:fggs.php?page=SuggestProject&io=1");
-}
-?>
--- docs/Partials/protect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//checking if the session exists
-//if the session is not set it redirects the user to the login form/page
-  if(!isset($_SESSION["useruid"])){
-      header("location:login.php");
-  }
-?>
